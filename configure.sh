37d6ecec54d8b2c0420211abf4703f2ee9e9e725
Dercury <dercury@qq.com>
2018-06-25 17:28:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,73 +1,49 @@
 #!/bin/bash
 ##功能描述：
-#   构建配置
+#   构建框架
 ##外部依赖：
-#   ${BUILD_PATH}: 构建路径
+#   $BUILD_PATH: 构建路径
 
-function PrintHelp
-{
-    echo  "Please refer to README."
-    exit $1
-}
-
-echo
-echo "-------------------- input config -----------------------"
-
-while [ -n "$1" ]
-do
-    case "$1" in
-    --target) 
-        TARGET="$2"
-        echo TARGET=$TARGET
-        shift;;
-    --src-file) 
-        SRC_FILE="$2"
-        echo SRC_FILE=$SRC_FILE
-        shift;;
-    --include-path) 
-        INCLUDE_PATH="$2"
-        echo INCLUDE_PATH=$INCLUDE_PATH
-        shift;;
-    --make-option) 
-        MAKE_OPTION="$2"
-        echo MAKE_OPTION=$MAKE_OPTION
-        shift;;
-    --release-option) 
-        RELEASE_OPTION="$2"
-        echo RELEASE_OPTION=$RELEASE_OPTION
-        shift;;
-    --all-config) 
-        ALL_CONFIG="$2"
-        echo ALL_CONFIG=$ALL_CONFIG
-        shift;;
-    --ext-config) 
-        EXT_CONFIG="$2"
-        echo EXT_CONFIG=$EXT_CONFIG
-        shift;;
-    --ext-make) 
-        EXT_MAKE="$2"
-        echo EXT_MAKE=$EXT_MAKE
-        shift;;
-    --ext-release) 
-        EXT_RELEASE="$2"
-        echo EXT_RELEASE=$EXT_RELEASE
-        shift;;
-    --help) 
-        PrintHelp 0;;
-    *) 
-        echo "Warning: $1 is not support.";;
-    esac
-    shift
-done
-
+# dirname返回的是非标准化路径，故需要通过cd+pwd的方式来进一步获取标准路径
+# 在调用脚本中直接使用时，$0与$BASH_SOURCE没有区别；但在被调用脚本souce时，$0给出的是调用脚本路径，而$BASH_SOUCE给出的被souce脚本路径。
+export DO_MAKE_PATH=$(cd $(dirname $BASH_SOURCE); pwd)
+echo DO_MAKE_PATH=$DO_MAKE_PATH
 echo
 
-if [ $ALL_CONFIG ]; then
-    # 使用统一mak脚本
-    echo 1
-elif [[ $TARGET && $SRC_FILE ]]; then
-    # 使用分散mak脚本
-    echo 2
+# 检查外部依赖的构建路径
+if [ -z "$BUILD_PATH" ]; then
+    echo "Error: BUILD_PATH is NOT specified."
+    exit 1
 else
-    PrintHelp 1
+    echo "Using BUILD_PATH=$BUILD_PATH"
 fi
+
+# 构建配置
+source $DO_MAKE_PATH/configure.sh
+if (( $? )); then
+    echo "Configure ...... Failed."
+    exit $?
+else
+    echo "Configure ...... Successed."
+fi
+
+# 执行编译
+# 这里必须用source，否则configure.sh中定义的变量传递不出来
+source $DO_MAKE_PATH/do_make.sh
+if (( $? )); then
+    echo "Make ...... Failed."
+    exit $?
+else
+    echo "Make ...... Successed."
+fi
+
+# 发布版本
+# 这里必须用source，否则configure.sh中定义的变量传递不出来
+source $DO_MAKE_PATH/release.sh
+if (( $? )); then
+    echo "Release ...... Failed."
+    exit $?
+else
+    echo "Release ...... Successed."
+fi
+
